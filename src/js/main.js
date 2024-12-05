f187d185954b8a20be88a82aafc321b80d340882
sophia.c <schen4333@gmail.com>
2024-12-05 14:48:43 -0500

    basic layout is established. basic drag-and-drop schedule is done.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,41 @@
+let workVal = 100;
+let productVal = 90;
+let physHealthVal = 37;
+let mentHealthVal = 20;
+
 $(document).ready(function () {
     // jQuery methods go here...
+    $(function () {
+        $("#schedule").sortable();
+    });
+
+    $(".scheduleBlocks").each(function () {
+        $(this).draggable({
+            connectToSortable: "#schedule",
+            helper: "clone",
+            revert: "invalid"
+        });
+    });
+
+    $(function () {
+        $("#workload").progressbar({
+            value: workVal
+        });
+    });
+    $(function () {
+        $("#productivity").progressbar({
+            value: productVal
+        });
+    });
+    $(function () {
+        $("#physHealth").progressbar({
+            value: physHealthVal
+        });
+    });
+    $(function () {
+        $("#mentHealth").progressbar({
+            value: mentHealthVal
+        });
+    });
 
 });
